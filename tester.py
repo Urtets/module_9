37fcb0c4f5b151a1ce2085a8e8e1e92311601bbb
Юрий <urtets@gmail.com>
2024-10-31 17:52:56 +0300

    Module_9 homework

--- tester.py
+++ tester.py
@@ -1,9 +1,15 @@
-def is_odd(x):
-    return x % 2
+import time
 
+list_1 = [1, 5, 9, 29, 4]
+list_2 = [5, 2, 9, 1, 2]
 
-my_numbers = [3, 1, 4, 1, 5, 9, 2, 6]
+ran = range(10, 30)
+zp = zip(list_1, list_2)
+mp = map(str, list_1)
 
-result = filter(is_odd, my_numbers)
-print(result)
-print(list(result))
+print(ran, zp, mp)
+
+print(list(ran))
+print(list(zp))
+print(list(mp))
+
